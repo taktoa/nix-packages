b8b7dec83b68f06d828263174e905838dee92866
Remy Goldschmidt <taktoa@gmail.com>
2016-09-29 20:07:07 -0500

    updated

--- git-cred-gnome/default.nix
+++ git-cred-gnome/default.nix
@@ -5,10 +5,11 @@ stdenv.mkDerivation {
   src = "${git}/share/git/contrib/credential/gnome-keyring";
   buildInputs = [ pkgconfig glib libgnome_keyring ];
   patchPhase = ''
-    sed -i 's|#include <glib.h>|#include <glib-2.0/glib.h>|g' git-credential-gnome-keyring.c
+    sed -i 's|#include <glib.h>|#include <glib-2.0/glib.h>|g' \
+        git-credential-gnome-keyring.c
   '';
   installPhase = ''
-    mkdir -p $out/share/git/contrib/credential/gnome-keyring/
-    install git-credential-gnome-keyring $out/share/git/contrib/credential/gnome-keyring/
+    mkdir -p $out/bin/
+    install git-credential-gnome-keyring $out/bin/
   '';
 }
--- pythonPackages/default.nix
+++ pythonPackages/default.nix
@@ -4,189 +4,168 @@ with builtins;
 
 with pythonPackages;
 
-with pkgs.lib;
+with { inherit (pkgs) lib; };
 
-let lib      = pkgs.lib;
-    notNull  = x: !(isNull x);
-    condNull = v: x: y: if isNull v then x else y;
-    bindNull = v: f: condNull v null (f v);
-    optNull  = value: default: condNull value default value;
-    optAttr  = key: value: condNull value {} { "${key}" = value; };
-    removeNulls = filterAttrs (n: v: notNull v);
-    mkPython = let srcHelper = url: sha256: curlOpts: name:
-                                 assert (notNull url && notNull sha256);
-                                 pkgs.fetchurl (removeNulls {
-                                   inherit url sha256 name curlOpts;
-                                 });
-               in { packageName         # < A package name.
-                  , version             # < A package version.
-                  , srcURL       ? null # < A url to download from.
-                  , srcSHA       ? null # < A SHA256 checksum for the download.
-                  , srcName      ? null # < A name for the downloaded file.
-                  , srcCurlOpts  ? null # < A curl options for the download.
-                  , src          ? (srcHelper srcURL srcSHA srcCurlOpts srcName)
-                                        # ^ A source to use, if url and sha256
-                                        # | are not provided.
-                  , buildDeps    ? []   # < Build dependencies to add.
-                  , propDeps     ? []   # < Propagated dependencies to add.
-                  # ! The following attributes are equivalent to those in meta.
-                  , homepage     ? null # < A homepage of the project.
-                  , description  ? null # < A description.
-                  , longDesc     ? null # < A longer description.
-                  , maintainers  ? null # < A list of maintainers.
-                  , license      ? null # < A license.
-                  , platforms    ? null # < A list of supported platforms.
-                  , ...
-                  } @ args:
-                    let 
-                    in buildPythonPackage (removeNulls ({
-                         name = "${packageName}-${version}";
+let removeNulls = lib.filterAttrs (n: v: v != null);
+    srcHelper = url: sha256: curlOpts: name:
+      assert url != null;
+      assert sha256 != null;
+      pkgs.fetchurl (removeNulls { inherit url sha256 name curlOpts; });
+    
+    mkPython =
+      { packageName            # < A package name.
+      , version                # < A package version.
+      , srcURL          ? null # < A url to download from.
+      , srcSHA          ? null # < A SHA256 checksum for the download.
+      , srcName         ? null # < A name for the downloaded file.
+      , srcCurlOpts     ? null # < A curl options for the download.
+      , src             ? null # < A source to use (overrides other options).
+      , buildDeps       ? []   # < Build dependencies to add.
+      , propDeps        ? []   # < Propagated dependencies to add.
+      , nativeBuildDeps ? []   # < Native build dependencies to add.
+      , nativePropDeps  ? []   # < Native propagated dependencies to add.
+      # The following attributes are equivalent to those in meta:
+      , homepage        ? null # < A homepage of the project.
+      , description     ? null # < A description.
+      , longDescription ? null # < A longer description.
+      , maintainers     ? null # < A list of maintainers.
+      , license         ? null # < A license.
+      , platforms       ? null # < A list of supported platforms.
+      , ... # Any other options will be passed to buildPythonPackage directly
+      } @ args:
+      
+      buildPythonPackage (removeNulls ({
+        name = "${packageName}-${version}";
 
-                         inherit src;
+        src = if isNull src
+              then srcHelper srcURL srcSHA srcCurlOpts srcName
+              else src;
 
-                         buildInputs = buildDeps;
-                         propagatedBuildInputs = propDeps;
+        buildInputs           = buildDeps;
+        propagatedBuildInputs = propDeps;
 
-                         meta = removeNulls { inherit homepage
-                                                      description
-                                                      license
-                                                      maintainers
-                                                      platforms;
-                                              longDescription = longDesc;
-                                            };
-                       } // args));
-   isNegative    = x: 0 >  x;
-   isNonPositive = x: 0 >= x;
-   isZero        = x: 0 == x;
-   isNonNegative = x: 0 <= x;
-   isPositive    = x: 0 <  x;
-   isNonEmpty       = x: isPositive (length x);
-   isNonEmptyString = x: isPositive (stringLength x);
+        nativeBuildInputs           = nativeBuildDeps;
+        propagatedNativeBuildInputs = nativePropDeps;
+
+        meta = removeNulls {
+          inherit homepage description longDescription
+                  license maintainers platforms;
+        };
+      } // args));
+   isNonEmptyString = x: stringLength x > 0;
    strHead = x: (assert isNonEmptyString x; substring 0 1 x);
-   pypiRoot = "https://pypi.python.org/packages/source";
    pypiURL = name: version:
-               assert isNonEmptyString name;
-               assert isNonEmptyString version;
-               "${pypiRoot}/${strHead name}/${name}/${name}-${version}.tar.gz";
+     assert isNonEmptyString name;
+     assert isNonEmptyString version;
+     "mirror://pypi/${strHead name}/${name}/${name}-${version}.tar.gz";
+
 in pythonPackages // (rec {
-     # bobbuilder = pkgs.callPackage ./bobbuilder {};
+  # bobbuilder = pkgs.callPackage ./bobbuilder {};
 
-     # Required for pylint
-     astroid = mkPython (rec {
-       packageName  = "astroid";
-       version      = "1.3.8";
-       srcURL       = pypiURL packageName version;
-       srcSHA       = "0hllf58wyz7mi7582psvswxgrybxidsv2vk3xgyarisxcxfc6w9r";
-       propDeps     = with pkgs; [ logilab_common six ];
-       homepage     = http://www.astroid.org;
-       description  = "Python AST parser";
-       license      = null; # e.g.: lib.licenses.mit
-       maintainers  = with lib.maintainers; [ taktoa ];
-       platforms    = with lib.platforms; all;
-     });
-          
-     pylint = mkPython (rec {
-       packageName  = "pylint";
-       version      = "1.4.4";
-       srcURL       = pypiURL packageName version;
-       srcSHA       = "08phsbbv17zaqaw9vvxwaxy1ixw46vlpz6gk0a79xynldd6syq85";
-       propDeps     = with pkgs; [ astroid ];
-       homepage     = https://github.com/hfaran/pylint;
-       description  = "A bug and style checker for Python";
-       license      = null; # e.g.: lib.licenses.mit
-       maintainers  = with lib.maintainers; [ taktoa ];
-       platforms    = with lib.platforms; all;
-       
-       postInstall = ''
-         mkdir -p $out/share/emacs/site-lisp
-         cp "elisp/"*.el $out/share/emacs/site-lisp/
-       '';
-     });
+  theano-deep = pythonPackages.mkPython (rec {
+    packageName  = "theano-deep";
+    version      = "0.8.2";
+    srcURL       = pypiURL "theano" version;
+    srcSHA       = "0hllf58wyz7mi7582psvswxgrybxidsv2vk3xgyarisxcxfc6w9r";
+    propDeps     = with self; [ logilab_common six ];
+    homepage     = http://deeplearning.net/software/theano/;
+    description  = "Optimizing compiler for evaluating mathematical expressions on CPUs and GPUs.";
+    license      = lib.licenses.bsd;
+    maintainers  = with lib.maintainers; [ andrew ];
+    platforms    = with lib.platforms; all;
+  });
+    
+  typed-ast = mkPython (rec {
+    packageName  = "typed-ast";
+    version      = "0.5.3";
+    srcURL       = pypiURL packageName version;
+    srcSHA       = "0ybssbjbmx311w9jyix1ig9kkhn85n1yy0br1sa8ipp9ajgs8dcn";
+    propDeps     = with pkgs; [];
+    homepage     = "https://github.com/dropbox/typed_ast";
+    description  = "A typed AST for Python";
+    license      = with lib.licenses; [ asl20.spdxId ];
+    maintainers  = with lib.maintainers; [ taktoa ];
+    platforms    = with lib.platforms; all;
+  });
+  
+  mypy-lang = mkPython (rec {
+    packageName  = "mypy-lang";
+    version      = "0.4.1";
+    srcURL       = pypiURL packageName version;
+    srcSHA       = "0xqvp88fmjbykjdcp0a2smlnmhsqz8m6x96drc34v9bkqms29vhw";
+    propDeps     = with pkgs; [ typed-ast ];
+    homepage     = "http://mypy-lang.org";
+    description  = "Experimental static types for Python";
+    license      = with lib.licenses; [ mit.spdxId ];
+    maintainers  = with lib.maintainers; [ taktoa ];
+    platforms    = with lib.platforms; all;
+  });
 
-     mypy-lang = mkPython (rec {
-       packageName  = "mypy-lang";
-       version      = "0.2.0";
-       srcURL       = pypiURL packageName version;
-       srcSHA       = "0qy95x79plddlvycm0l9dr1m23hmacb8sxlv6z8l4p9c2wjagyf7";
-       propDeps     = with pkgs; [];
-       homepage     = http://mypy-lang.org;
-       description  = "Experimental static types for Python";
-       license      = null; # e.g.: lib.licenses.mit
-       maintainers  = with lib.maintainers; [ taktoa ];
-       platforms    = with lib.platforms; all;
-     });
+  piazza-api = mkPython (rec {
+    packageName  = "piazza-api";
+    version      = "0.5.2";
+    srcURL       = pypiURL packageName version;
+    srcSHA       = "0w3pkxx4b8rh82qqzl9nag4r8dmc5nbngf3afjlml5dmbgjpv23z";
+    propDeps     = with pkgs; [
+                     blockdiag docutils reportlab
+                     pep8 nose pip pillow requests
+                   ];
+    homepage     = "https://github.com/hfaran/piazza-api";
+    description  = "An unofficial client for Piazza's internal API";
+    licenses     = with lib.licenses; [ mit.spdxId ];
+    maintainers  = with lib.maintainers; [ taktoa ];
+    platforms    = with lib.platforms; all;
+  });
 
-     piazza-api = mkPython (rec {
-       packageName  = "piazza-api";
-       version      = "0.5.1";
-       srcURL       = pypiURL packageName version;
-       srcSHA       = "1vr4hnkr0gcvvpaycd8q3vcx029b2f5yv8swhdr8kwspaqb0dvfa";
-       propDeps     = with pkgs; [ blockdiag docutils reportlab
-                                   pep8 nose pip pillow
-                                 ];
-       homepage     = https://github.com/hfaran/piazza-api;
-       description  = "An unofficial client for Piazza's internal API";
-       licenses     = with lib.licenses; [ mit ];
-       maintainers  = with lib.maintainers; [ taktoa ];
-       platforms    = with lib.platforms; all;
-     });
+  scdl = mkPython (rec {
+    packageName  = "scdl";
+    version      = "1.4.1";
+    srcSHA       = "005him6q85q3026xrsdkzawwl3n1fcchi481fj2daivrl9wrrpjb";
+    propDeps     = with pkgs; [];
+    homepage     = "https://github.com/flyingrub/scdl";
+    description  = "SoundCloud music downloader.";
+    license      = with lib.licenses; [ gpl2.spdxId ];
+    maintainers  = with lib.maintainers; [ taktoa ];
+    platforms    = with lib.platforms; all;
+  });
 
-     # Required for csvkit
-     dateutil_2_2 = mkPython (rec {
-       packageName  = "dateutil";
-       version      = "2.2";
-       srcURL       = pypiURL "python-dateutil" version;
-       srcSHA       = "0s74ad6r789810s10dxgvaf48ni6adac2icrdad34zxygqq6bj7f";
-       propDeps     = with pkgs; [ six ];
-       homepage     = http://pypi.python.org/pypi/python-dateutil;
-       description  = "Powerful extensions to the standard datetime module";
-       license      = lib.licenses.bsd2.spdxId;
-       maintainers  = with lib.maintainers; [ taktoa ];
-       platforms    = with lib.platforms; all;
-     });
+  ptpython = mkPython (rec {
+    packageName  = "ptpython";
+    version      = "0.34";
+    srcURL       = pypiURL packageName version;
+    srcSHA       = "1mmbiyzf0n8hm7z2a562x7w5cbl6jc0zsk6vp40q1z4cyblv1k13";
+    propDeps     = with pkgs; [ jedi docopt pygments prompt_toolkit ];
+    homepage     = "https://github.com/jonathanslenders/ptpython";
+    description  = "A more advanced Python REPL.";
+    license      = with lib.licenses; [ bsd3.spdxId ];
+    maintainers  = with lib.maintainers; [ taktoa ];
+    platforms    = with lib.platforms; all;
+    doCheck      = false;
+  });
 
-     # Required for csvkit
-     openpyxl_2_2_0-b1 = mkPython (rec {
-       packageName  = "openpyxl";
-       version      = "2.2.0-b1";
-       srcURL       = pypiURL packageName version;
-       srcSHA       = "0n10pawp2558jrrmppyhkrv7889k3g4mifqj3fp68qbr20ldk51k";
-       buildDeps    = with pkgs; [ pytest ];
-       propDeps     = with pkgs; [ jdcal et_xmlfile ];
-       description  = "A Python library to read/write Excel 2007 files";
-       homepage     = https://openpyxl.readthedocs.org;
-       license      = lib.licenses.mit.spdxId;
-       maintainers  = with lib.maintainers; [ taktoa ];
-       platforms    = with lib.platforms; all;
-     });
-     
-     csvkit = mkPython (rec {
-       packageName  = "csvkit";
-       version      = "0.9.1";
-       srcURL       = pypiURL packageName version;
-       srcSHA       = "0fprr4wgp0bq8kl5qims88np11af7ahr5bxkrhfwpdgcgdjbiy4j";
-       propDeps     = with pkgs; [ sqlalchemy
-                                   xlrd
-                                   openpyxl_2_2_0-b1
-                                   dateutil_2_2
-                                 ];
-       homepage     = https://github.com/onyxfish/csvkit;
-       description  = "A suite of tools for dealing with CSV";
-       license      = null; # e.g.: lib.licenses.mit
-       maintainers  = with lib.maintainers; [ taktoa ];
-       platforms    = with lib.platforms; all;
-     });
-     
-     scdl = mkPython (rec {
-       packageName  = "scdl";
-       version      = "1.4.1";
-       srcURL       = pypiURL packageName version;
-       srcSHA       = "005him6q85q3026xrsdkzawwl3n1fcchi481fj2daivrl9wrrpjb";
-       propDeps     = with pkgs; [ ];
-       homepage     = https://github.com/flyingrub/scdl;
-       description  = "SoundCloud music downloader.";
-       license      = with lib.licenses; [ gpl2.spdxId ];
-       maintainers  = with lib.maintainers; [ taktoa ];
-       platforms    = with lib.platforms; all;
-     });
-   })
+  xmlutils = mkPython (rec {
+    packageName  = "xmlutils";
+    version      = "1.3";
+    srcURL       = pypiURL packageName version;
+    srcSHA       = "0z03bai6a9gfl6hy8cracb88i51ghczn8xdxsxj7ayc7kjskm2id";
+    propDeps     = with pkgs; [ ];
+    homepage     = "https://nadh.in/code/xmlutils.py";
+    description  = "Tools for converting XML to JSON, CSV, and other formats.";
+    license      = with lib.licenses; [ mit.spdxId ];
+    maintainers  = with lib.maintainers; [ taktoa ];
+    platforms    = with lib.platforms; all;
+  });
+
+  jsonwidget = mkPython (rec {
+    packageName  = "jsonwidget";
+    version      = "0.1.7";
+    srcURL       = pypiURL packageName version;
+    srcSHA       = "1i8jm0sijlivak4rmhdan51w2mhz3h55xg7jcvlsz1zc8hz5mkw3";
+    propDeps     = with pkgs; [ simpleparse urwid ];
+    homepage     = "http://robla.net/jsonwidget";
+    description  = "A general-purpose JSON validation and manipulation, and form-building library.";
+    license      = with lib.licenses; [ lgpl21.spdxId ];
+    maintainers  = with lib.maintainers; [ taktoa ];
+    platforms    = with lib.platforms; all;
+  });
+})
